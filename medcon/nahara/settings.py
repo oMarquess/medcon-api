9a920b333ff7b19ead50a78f1a4158325ef9b97a
oMarquess <pymasterclass@gmail.com>
2024-01-11 03:29:51 +0000

    update

--- medcon/nahara/settings.py
+++ medcon/nahara/settings.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:efea4b8ca5b20e408836fac2e598860ca68324dda10eca09d06f0a15cfb7c45b
-size 4211
+oid sha256:0337850ed2a3a7dabb2e05903ba1212b3f9ebdcda100eec2ecaba50af6e17264
+size 4210
